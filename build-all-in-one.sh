634dbe111a130d18ccbbaac575614e171247d5c5
肖盛文 <atzlinux@yeah.net>
2019-10-07 12:15:06 +0800

    忽略所有 git 文件，压缩包文件增加目录名，解压缩时才可以生成 debian-cn 目录

--- build-all-in-one.sh
+++ build-all-in-one.sh
@@ -2,6 +2,7 @@
 
 VDATE=`git log -1 --pretty=format:"%ad" --date=short`
 
+rm -fv all-in-one.version.*.txt
 git log -1 > all-in-one.version.$VDATE.txt
 
-tar czvf /tmp/debian-cn.tar.gz --exclude=".git"  *
+tar czvf /tmp/debian-cn.tar.gz --exclude=".git*" ../debian-cn
