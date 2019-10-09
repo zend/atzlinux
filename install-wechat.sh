e2441071c3507006c2f2bbdc0639ed4ea245b375
肖盛文 <atzlinux@yeah.net>
2019-10-09 16:02:10 +0800

    更新微信安装信息

--- install-wechat.sh
+++ install-wechat.sh
@@ -2,7 +2,7 @@
 cd wechat
 cd `dirname $0`; pwd
 echo '即将开始安装 ...'
-#wget -c -O electronic-wechat_2.0.1_amd64.deb  http://archive.ubuntukylin.com/ubuntukylin/pool/main/e/electronic-wechat/electronic-wechat_2.0.1_i386.deb 
+wget -c -O electronic-wechat_2.0.1_i386.deb  http://archive.ubuntukylin.com/ubuntukylin/pool/main/e/electronic-wechat/electronic-wechat_2.0.1_i386.deb 
 
 apt -y install ./electronic-wechat_2.0.1_i386.deb
 
