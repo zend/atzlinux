5ac12c4bf439d0ab86fc6699178afa5cf1e8418f
肖盛文 <atzlinux@yeah.net>
2019-10-09 16:08:43 +0800

    更新 wine32 安装信息

--- install-wine32.sh
+++ install-wine32.sh
@@ -3,8 +3,7 @@ cd `dirname $0`; pwd
 echo '即将开始安装  ...'
 ./install-deepin-keyring.sh  
 ./deepin/add-deepin-repo.sh
-#apt -y install deepin-wine deepin-wine-helper:i386 deepin-wine-plugin deepin-wine-plugin-virtual deepin-wine-uninstaller:i386 deepin-wine32-preloader:i386
-dpkg -i ./deepin-wine32/*i386.deb ./deepin-wine32/*all.deb
+apt -y install deepin-wine deepin-wine-helper:i386 deepin-wine-plugin deepin-wine-plugin-virtual deepin-wine-uninstaller:i386 deepin-wine32-preloader:i386
 echo '安装完成，正在自动安装依赖...'
 apt -f -y install
 ./deepin/uninstall-deepin-keyring.sh
