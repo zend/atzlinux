56d07deb08a8ed8efcc5e6cdfa89095abf93fc11
肖盛文 <atzlinux@yeah.net>
2019-10-09 15:50:42 +0800

    删除 all-in-one 分支文件

--- build-all-in-one.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VDATE=`git log -1 --pretty=format:"%ad" --date=short`
-
-rm -fv all-in-one.version.*.txt
-git log -1 > all-in-one.version.$VDATE.txt
-
-tar czvf /tmp/debian-cn.tar.gz --exclude=".git*" ../debian-cn
