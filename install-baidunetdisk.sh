7ca41f0480b0dac62c8c801942c27c518f40f01e
肖盛文 <atzlinux@yeah.net>
2019-10-09 11:45:09 +0800

    删除不支持 32 位的 百度网盘、网易云音乐 更新 wps、搜狗拼音、有道词典 相关信息

--- install-baidunetdisk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd `dirname $0`; pwd
-echo '即将开始安装 ...'
-
-# 安装 update-desktop-database 命令
-apt -y install desktop-file-utils
-
-apt -y install ./baidunetdisk/baidunetdisk_linux_2.0.2.deb
--- install-youdao-dict.sh
+++ install-youdao-dict.sh
@@ -3,8 +3,9 @@ cd youdao-dict
 cd `dirname $0`; pwd
 echo '即将开始安装 ...'
 #wget -c -O youdao-dict_1.1.0-0-deepin_amd64.deb http://codown.youdao.com/cidian/linux/youdao-dict_1.1.0-0-deepin_amd64.deb
+wget -c -O youdao-dict_1.1.0-0-deepin_i386.deb "http://codown.youdao.com/cidian/linux/youdao-dict_1.1.0-0-deepin_i386.deb"
 
-apt -y install ./youdao-dict_1.1.0-0-deepin_amd64.deb
+apt -y install ./youdao-dict_1.1.0-0-deepin_i386.deb
 
 patch -f -p 1 /usr/share/youdao-dict/dae/ewmh.py < youdao-dict.patch
 
