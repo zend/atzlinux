f67249e8b49c990d278083bb154f25c99a1f33b7
肖盛文 <atzlinux@yeah.net>
2019-10-09 12:42:11 +0800

    更新 i386 卸载脚本

--- uninstall.sh
+++ uninstall.sh
@@ -14,12 +14,6 @@ echo "开始卸载  wine32 ..."
 echo "开始卸载 wechat ..."
 ./wechat/uninstall-wechat.sh
 
-echo "开始卸载 baidunetdisk ..."
-./baidunetdisk/uninstall-baidunetdisk.sh
-
-echo "开始卸载 netease-cloud-music ...."
-./netease-cloud-music/uninstall-netease-cloud-music.sh
-
 echo "开始卸载 youdao-dict ..."
 ./youdao-dict/uninstall-youdao-dict.sh
 
