77146e6bb06b67e761d3a4b2c5586d4ed11071b6
patternos <pattern@auto365.online>
2022-04-06 10:06:30 +0800

    auto commit

--- 1.ci.caller.sh
+++ 1.ci.caller.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-
-#打开执行过程显示
 set +x
-#显示设置环境变量 CMD_PATH当前脚本所在目录
 export CMD_PATH=$(cd `dirname $0`; pwd)
-
 cd $CMD_PATH
-
 ./2.ci.expect.sh
--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -11,14 +11,3 @@ apt install simple-cdd -y
 cd isodvd
 rm -rf tmp/mirror/db/lockfile
 ./amd64.build.sh
-
-# 1007.4.deb.control.gen.sh
-# 1007.5.deb.make.sh
-# 1007.6.deb.install.sh
-
-# 1007.19.docker.build.sh
-# 1007.20.docker.push.sh
-# 1007.21.kompose.convert.sh
-# 1007.22.k8s.deploy.dev.sh
-
-# 1007.23.k8s.deploy.qingdao.sh
