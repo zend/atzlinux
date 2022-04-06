2990cb8a625c51a68719c461a87e36fa7860cb1c
act365 <gnuhub@qq.com>
2022-04-06 14:51:38 +0800

    增加镜像源环境变量便于切换定义镜像源 auto commit

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -7,4 +7,7 @@ cd $CMD_PATH
 apt install simple-cdd -y
 cd isodvd
 rm -rf tmp/mirror/db/lockfile
+export atzlinux_debian_mirror_extra="https://apt.atzlinux.com/atzlinux"
+export atzlinux_debian_security_mirror="https://mirrors.huaweicloud.com/debian-security/ "
+export atzlinux_debian_mirror="https://mirrors.huaweicloud.com/debian/"
 ./amd64.build.sh
--- isodvd/amd64.build.sh
+++ isodvd/amd64.build.sh
@@ -1,1 +1,17 @@
-build-simple-cdd --force-root --debug --dvd --proposed-updates --dist buster --locale zh_CN --keyboard us --security-mirror https://mirrors.huaweicloud.com/debian-security/ --debian-mirror https://mirrors.huaweicloud.com/debian/  -b amd64.build
+#!/usr/bin/env bash
+
+set -x
+
+export atzlinux_debian_mirror_extra=${atzlinux_debian_mirror_extra:="http://txy.atzlinux.com/atzlinux"}
+export atzlinux_debian_security_mirror=${atzlinux_debian_security_mirror:="https://mirrors.huaweicloud.com/debian-security/ "}
+export atzlinux_debian_mirror=${atzlinux_debian_mirror:="https://mirrors.huaweicloud.com/debian/"}
+
+build-simple-cdd --force-root \
+--debug  \
+--dvd \
+--dist buster \
+--locale zh_CN \
+--keyboard us  \
+--security-mirror ${atzlinux_debian_mirror_extra} \
+--debian-mirror ${atzlinux_debian_mirror}  \
+-b amd64.build
