7d2b5598e35fbc4e0c97c80f4450b4fe81ad5b59
肖盛文 <atzlinux@yeah.net>
2023-06-03 15:23:43 +0800

    update os-pkg-update.sh, temp add experimental

--- isodvd/profiles/os-pkg-update.sh
+++ isodvd/profiles/os-pkg-update.sh
@@ -6,8 +6,9 @@ pooldir=`apt-cache showsrc $p|grep ^Directory:|awk {'print $2'}|tail -n 1`
 echo $pooldir
 mkdir -pv $pooldir
 cd $pooldir
+apt-get download $p/experimental
 apt-get download $p
-apt-get -o Dir::Cache::Archives=/home/atzlinux/a11test/atzlinux/$pooldir autoclean
+apt-get -o Dir::Cache::Archives=/home/atzlinux/a11/atzlinux/$pooldir autoclean
 cd -
 done
 
