a30f282036470a2f542521c8b5e2dcbf9356f2cb
xiao sheng wen(肖盛文) <xsw@atzlinux.com>
2023-06-26 11:38:37 +0800

    add a11 apt shell conf pkg.list
    Signed-off-by: xiao sheng wen(肖盛文) <xsw@atzlinux.com>

---
+++ a11/atzlinux/os-pkg-update.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+for p in `cat os-regular-update.list firmware.pkg.list`
+do
+pooldir=`apt-cache showsrc $p|grep ^Directory:|awk {'print $2'}|tail -n 1`
+echo $pooldir
+mkdir -pv $pooldir
+cd $pooldir
+#apt-get download $p/experimental
+apt-get download $p
+apt-get -o Dir::Cache::Archives=/home/atzlinux/a11/atzlinux/$pooldir autoclean
+cd -
+done
+
+# clear lock file from apt-get autoclean
+find ./ -name "lock" -exec rm -fv {} \;
