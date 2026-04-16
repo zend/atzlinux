1c6bac7eaa00bbec0c59bc6706b8e8d08648c2fd
Mike <combbs@gmail.com>
2026-04-16 23:40:21 +0800

    Use Ubuntu 22.04 runner - live-build has bug on 24.04

--- isodvd/livecd/build-livecd.sh
+++ isodvd/livecd/build-livecd.sh
@@ -254,7 +254,7 @@ configure_live_build() {
         --security "false" \
         --firmware-chroot "false" \
         --cache "false" \
-        --bootloader "syslinux" \
+        --bootloader "grub" \
         --mode "debian" \
         || {
             echo "Error: lb config failed"
